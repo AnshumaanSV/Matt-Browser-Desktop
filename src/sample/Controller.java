187159f13c9a45f4685263ccc8b5ec24adfaf236
AnshumaanSV <anshumaansv@gmail.com>
2019-08-27 16:02:54 +0530

    Code improvement

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
@@ -11,17 +12,14 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-
-    String htLink = "http://";
-
     @FXML
     TextField addressBar;
-
-    String adrsLink;
 
     @FXML
     WebView web;
 
+    String htLink = "http://";
+    String adrsLink;
     WebEngine engine;
 
     public void go(ActionEvent actionEvent) {
@@ -29,10 +27,16 @@ public class Controller implements Initializable {
         engine.load(htLink + adrsLink);
     }
 
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         engine = web.getEngine();
         engine.load(htLink + "www.google.com");
+
+        addressBar.setOnKeyReleased(e -> {
+            if (e.getCode() == KeyCode.ENTER){
+                adrsLink = addressBar.getText();
+                engine.load(htLink + adrsLink);
+            }
+        });
     }
 }
